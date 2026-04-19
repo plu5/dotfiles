d25e9e5e9f55f8c3bb489d420d2425e66c888cca
plus <plu5@users.noreply.github.com>
2026-04-19 12:21:36 +0300

    feat(metaf): Add --update option

--- pm/scripts/metaf.py
+++ pm/scripts/metaf.py
@@ -86,9 +86,45 @@ def get_file_type(path):
     return c.stdout.decode().split()[-1]
 
 
-def get_file_information(path, fmt):
-    # type: (str) -> dict
+def fields_to_update(path, fmt, existing):
+    # type: (str, str, dict) -> str
+    ret = []
+    m = existing.get(FORMATOPTIONS['m'])
+    if not m:
+        msg(f"{path} no modification date in data, updating all fields.")
+        return fmt              # update everything
+    new_m = get_file_modification_epoch(path)
+    if new_m < m:               # older
+        msg(f"{path} new modification date is older than existing one. How is "
+            f"this possible? ({new_m} < {m}). Keeping data as is.")
+    elif new_m > m:             # newer
+        msg(f"File {path} changed ({new_m} > {m})")
+        return fmt              # update everything
+    else:                       # equal
+        # check if there are new fields in fmt
+        for k, v in FORMATOPTIONS.items():
+            if k in fmt and v not in existing:
+                ret.append(k)
+        if len(ret):
+            msg(f"File {path} hasn't changed, but new fields requested {ret}")
+    return ''.join(ret)
+
+
+def get_file_information(path, fmt, existing=None):
+    # type: (str, str, dict | None) -> dict
+    """Get information about file in PATH. FMT defines which
+    information to get. EXISTING files data can be provided from a
+    previous run to update it if FMT contain fields not there or if
+    the data contains a modification date older than the file has on
+    disk, otherwise return it as is.
+    """
     res = {}
+
+    if existing:
+        fmt = fields_to_update(path, fmt, existing)
+        if not fmt:
+            return existing
+        res = existing
 
     c = get_file_creation_epoch(path) if 'c' in fmt.lower() else None
     m = get_file_modification_epoch(path) if 'm' in fmt.lower() else None
@@ -110,24 +146,51 @@ def get_file_information(path, fmt):
     return res
 
 
-def get_files_information_recursively(parent_path, fmt, include_subdirs=False):
-    # type: (str) -> dict
+def get_files_information_recursively(
+        parent_path, fmt, include_subdirs=False, existing=None):
+    # type: (str, str, bool, dict | None) -> dict
+    """Recurse PARENT_PATH and get information recursively about each
+    file. FMT defines which information to get about each file. If
+    INCLUDE_SUBDIRS, get information about subdirectories
+    too. EXISTING files data can be provided from a previous run to
+    use existing data unless there is a modification date listed and
+    it is older than the file has on disk, or FMT includes fields not
+    in the data.
+    """
     files_info_dict = {}
+
+    def process_file_or_subdir(root, name, is_subdir=False):
+        # type: (str, str, bool) -> None
+        p = os.path.join(root, name)
+        rel_p = p.removeprefix(parent_path + '/')
+        e = existing.get(rel_p) if existing else None
+        if existing and not e:
+            msg(f"File {p} is new")
+        try:
+            files_info_dict[rel_p] = get_file_information(p, fmt, e)
+        except FileNotFoundError as e:
+            if is_subdir:
+                raise e
+            else:
+                msg(f'{p} not found - broken symlink? Exception: {e}'
+                    '\nSkipping.')
+
     for root, subdirs, files in os.walk(parent_path):
         if include_subdirs:
             for f in subdirs:
-                p = os.path.join(root, f)
-                rel_p = p.removeprefix(parent_path + '/')
-                files_info_dict[rel_p] = get_file_information(p, fmt)
+                process_file_or_subdir(root, f, is_subdir=True)
         for f in files:
-            p = os.path.join(root, f)
-            rel_p = p.removeprefix(parent_path + '/')
-            try:
-                files_info_dict[rel_p] = get_file_information(p, fmt)
-            except FileNotFoundError as e:
-                msg(f'{p} not found - broken symlink? Exception: {e}')
-                msg('Skipping.')
+            process_file_or_subdir(root, f)
+
     return files_info_dict
+
+
+def read_existing(path):
+    # type: str -> dict | None
+    existing = None
+    with open(path, "r") as f:
+        existing = json.load(f)
+    return existing
 
 
 def parse_args():
@@ -155,6 +218,16 @@ def parse_args():
         '-o', '--overwrite', action='store_true',
         help='Overwrite save file if it already exists. '
         'Ignored if -s/--save is not provided.')
+    parser.add_argument(
+        '-u', '--update', action='store_true',
+        help='Update save file if it already exists. '
+        'This will add new items and update items whose modification date has '
+        'changed. '
+        'Ignored if -s/--save is not provided. If both -o/--overwrite '
+        'and -u/--update are provided, overwrite will take precedence.')
+    # REMARK(plu5): Maybe add "exists" field on items so that when
+    # using --update, items that no longer exist on disk can have
+    # their "exists" field set to False.
     return parser.parse_args()
 
 
@@ -169,24 +242,37 @@ def main():
         msg(f'requires a valid path, \'{parent_path}\' doesn\'t seem to be')
         exit()
 
+    existing = None             # existing files data
+
     # Fail fast if save file already exists and overwrite is False
+    # unless update is True
     if args.save and not args.overwrite:
         save_to = args.save_to or os.path.join(parent_path, DEFAULTSAVENAME)
         if os.path.exists(save_to):
-            msg(f'Save file \'{save_to}\' already exists. '
-                'Run with -o/--overwrite if you wish to overwrite it. '
-                'Run with -t/--save-to to specify a different save file.')
-            exit()
+            if not args.update:
+                msg(f'Save file \'{save_to}\' already exists. '
+                    'Run with -u/--update if you wish to update it. '
+                    'Run with -o/--overwrite if you wish to overwrite it. '
+                    'Run with -t/--save-to to specify a different save file.')
+                exit()
+            # update
+            existing = read_existing(save_to)
+            try:
+                existing = existing['files']
+            except KeyError:
+                msg('Invalid existing data; expected key "files" in',
+                    existing)
+                exit()
 
     now = time.time()
     out = {'generated': readable_date_from_epoch(now), 'generated_epoch': now,
            'files': get_files_information_recursively(
-               parent_path, args.format, args.all)}
+               parent_path, args.format, args.all, existing)}
     dump = json.dumps(out, indent=2)
 
     if args.save:
         save_to = args.save_to or os.path.join(parent_path, DEFAULTSAVENAME)
-        open_as = 'w' if args.overwrite else 'x'
+        open_as = 'w' if args.overwrite or args.update else 'x'
         with open(save_to, open_as) as f:
             f.write(dump)
     else:
